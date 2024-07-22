d6842588e6d6a5738bdd8735f8978a304e0ba5ab
Martexpol <150294627+Martexpol@users.noreply.github.com>
2024-07-22 16:02:52 +0200

    Updates

---
+++ assets/HomePage-DVmgzbIv.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CiVmmi8x.js";const t={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function i(){return e.jsx("div",{style:t.container,children:e.jsx("h1",{style:t.title,children:"Phonebook welcome page "})})}export{i as default};
